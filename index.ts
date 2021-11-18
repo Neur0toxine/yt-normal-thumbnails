29480f0a1603f3ea890fb518c35a52ee573efdf2
Neur0toxine <pashok9825@gmail.com>
2021-11-18 16:16:24 +0300

    fix for the lates update & add license

--- index.ts
+++ index.ts
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         YouTube Normal Thumbnails
 // @namespace    http://greasyfork.org
-// @version      0.7.0
+// @version      0.7.1
 // @description  Restores normal thumbnails size
 // @author       NeoCortex
+// @license      MIT
 // @match        *://www.youtube.com/*
 // @match        *://youtube.com/*
-// @run-at       document-end
+// @run-at       document-start
 // @grant        none
 // ==/UserScript==
 
 (function () {
-    const perRowProperty = '--ytd-rich-grid-items-per-row';
-    const styleContent = `
+    const styles = `
     ytd-rich-grid-video-renderer[mini-mode] #video-title.ytd-rich-grid-video-renderer {
         font-size: 1.4rem;
         font-weight: 500;
@@ -22,102 +22,37 @@
     #avatar-link.ytd-rich-grid-video-renderer {
         display: none !important;
     }
-    `.trim();
-    const commonStyles = `
+
     ytd-video-renderer[use-prominent-thumbs] ytd-thumbnail.ytd-video-renderer {
         min-width: 120px !important;
         max-width: 240px !important;
     }
-    `.trim();
+    `.trim()
 
     class YoutubeThumbnailsFixer {
-        private oldPerRow?: number;
-        private observer?: MutationObserver;
-        private target?: HTMLElement;
-        private videoObserverConfig = {
-            attributes: true,
-            childList: false,
-            subtree: false
-        };
-
         constructor() {
-            this.installContentObserver();
-            this.installStyle(commonStyles);
+            this.replaceMathMin()
+            document.addEventListener("DOMContentLoaded", () => this.installStyle(styles))
         }
 
-        private installContentObserver(): void {
-            this.observer = new MutationObserver((mutationsList, observer) => {
-                this.target = document.querySelector('ytd-rich-grid-renderer') as HTMLElement;
-
-                if (null !== this.target) {
-                    this.observer?.disconnect();
-                    this.installStyle(styleContent);
-                    this.updateRowsCount();
-                    this.installVideoGridObserver();
+        private replaceMathMin(): void {
+            const origMathMin = Math.min
+            function modifiedMathMin () {
+                if (/calcElementsPerRow/img.test(Error().stack || '')) {
+                    return origMathMin.apply(Math, (arguments as unknown) as Array<number>) + 1
                 }
-            });
-            this.observer.observe(document.getElementById('content') as Node, {
-                attributes: false,
-                childList: true,
-                subtree: true
-            });
-        }
-
-        private installVideoGridObserver(): void {
-            if (this.isNil(this.target)) {
-                return;
+        
+                return origMathMin.apply(Math, (arguments as unknown) as Array<number>)
             }
-
-            this.observer = new MutationObserver((mutationsList, observer) => {
-                for (let mutation of mutationsList) {
-                    if (mutation.attributeName == 'style') {
-                        this.observer?.disconnect();
-                        this.updateRowsCount();
-                        this.observer?.observe(this.target as Node, this.videoObserverConfig);
-                    }
-                }
-            });
-            this.observer?.observe(this.target as Node, this.videoObserverConfig);
-            console.info(`[YouTube Normal Thumbnails] Changed to ${this.currentPerRow} thumbnails per row instead of ${this.oldPerRow}.`);
-        }
-
-        private updateRowsCount(): void {
-            if (this.isNil(this.target)) {
-                return;
-            }
-
-            try {
-                if (this.oldPerRow === undefined || this.oldPerRow == this.currentPerRow || this.oldPerRow === 0) {
-                    this.oldPerRow = this.currentPerRow;
-                    this.currentPerRow += 1;
-                }
-            } catch (e) {
-                console.warn(`[YouTube Normal Thumbnails] Cannot update thumbnails count: ${e}`)
-            }
+            Math.min = modifiedMathMin
         }
 
         private installStyle(contents: string): void {
-            let style = document.createElement('style');
-            style.innerHTML = contents;
-            document.body.appendChild(style);
-        }
-
-        private isNil(item: any): boolean {
-            return null === item || undefined === item;
-        }
-
-        get currentPerRow(): number {
-            if (this.isNil(this.target)) {
-                return 0;
-            }
-
-            return +(this.target as HTMLElement).style.getPropertyValue(perRowProperty);
-        }
-
-        set currentPerRow(value: number) {
-            this.target?.style.setProperty(perRowProperty, String(value));
+            let style = document.createElement('style')
+            style.innerHTML = contents
+            document.body.appendChild(style)
         }
     }
 
-    new YoutubeThumbnailsFixer();
+    new YoutubeThumbnailsFixer()
 })();
